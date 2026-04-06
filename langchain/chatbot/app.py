66bda1f0ba5cb9f19131a993ff0d092f93499ca3
SiddheshDaphane <siddhesh.daphane@gmail.com>
2026-04-06 08:33:29 -0500

    simple chatbot in langchain

--- langchain/chatbot/app.py
+++ langchain/chatbot/app.py
@@ -1,10 +1,36 @@
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 import streamlit as st
-import os 
+import os
+from pathlib import Path
 from dotenv import load_dotenv
 
+env_path = Path(__file__).resolve().parent.parent.parent / '.env'
 
-# print("App is working!")
+load_dotenv(dotenv_path=env_path)
+
+os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+# Prompt template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please response to the user queries"),
+        ("user", "Question: {question}")
+    ]
+)
+
+st.title('Langchain Demo with Gemini API')
+input_text = st.text_input("Search the topic u want")
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
