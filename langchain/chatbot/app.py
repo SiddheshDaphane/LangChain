0b5d8f0232a15cf3bc0a010ede7a90aacef588da
SiddheshDaphane <siddhesh.daphane@gmail.com>
2026-04-06 07:40:45 -0500

    Created a repo structure to start with application

---
+++ langchain/chatbot/app.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os 
+from dotenv import load_dotenv
+
+
+# print("App is working!")
